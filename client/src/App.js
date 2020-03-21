6c0ce693b0d37d8b6662c3f90d2fa77dd09e3d21
Khoa Tran <jacktran1799@gmaail.com>
2020-03-21 00:37:53 -0500

    finished everything beside test and self-study questions

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,36 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import WorldCupPlayerCard from "./components/WorldCupPlayerCard"
+import Toggle from './components/Toggle'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+
+class App extends React.Component{
+   constructor(){
+     super();
+     this.state={
+       data:[]
+     }
+   }
+   componentDidMount(){
+    fetch('http://localhost:5000/api/players')
+     .then(res=> res.json())
+     .then(data=>{ 
+     console.log(data)
+      this.setState({data: data})
+    })
+   }
+
+   render(){
+     return <>
+        
+        <Toggle />
+        <WorldCupPlayerCard data={this.state.data}/>
+            
+       </>
+   }
 }
 
 export default App;
